14be7d5e473681d63d5736a204bb1c8c74928ec5
AlejandroBel27 <ruben.saldana216100@potros.itson.edu.mx>
2025-05-20 13:56:38 -0700

    Terminacino del CRUD para las instalaciones. crea, actualiza y elimina los anuncios de instalacion.

--- admin/index.php
+++ admin/index.php
@@ -12,6 +12,32 @@
 
     // en caso de no tener un resultado le genera un valor nulo para evitar error.
     $resultado = $_GET['resultado'] ?? null;
+
+    //validar que el id Sea int y no algun otro dato para evitar inyeccion SQL
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if($id){
+            
+            //eliminar el archivo de la imagen.
+
+            $query="SELECT imagen FROM instalaciones WHERE id= ${id}";
+
+            $resultado = mysqli_query($db, $query);
+            $instalacion = mysqli_fetch_assoc($resultado);
+            unlink('../imagenes/' . $instalacion['imagen']);
+            
+            //Eliminar la instalacion.
+            $query = "DELETE FROM instalaciones WHERE id = ${id}";
+            $resultado = mysqli_query($db,$query);
+            if($resultado){
+                header('location: /admin?resultado=3');
+            }
+        }
+
+    }
+
     // incluye el template de header.php para que sea mas dinamico.
     require '../includes/funciones.php';
     incluirTemplate('header');
@@ -23,7 +49,9 @@
             <p class="alerta exito">Anuncio Creado Correctamente</p>
         <?php elseif(intval ($resultado) === 2) : ?>
             <p class="alerta exito">Anuncio Actualizado Correctamente</p>
-            <?php endif; ?>
+        <?php elseif(intval ($resultado) === 3) : ?>
+            <p class="alerta exito">Anuncio eliminado Correctamente</p>
+        <?php endif; ?>
 
         <a href="/admin/instalaciones/crear.php" class="boton boton-morado-inline-block">Nueva Instalación</a>
 
@@ -46,7 +74,11 @@
                     <td><img src="/imagenes/<?php echo $instalacion['imagen']?>" class="imagen-tabla"> </td>
                     <td>$ <?php echo $instalacion['precio']?> mxn</td>
                     <td>
-                        <a href="admin/instalaciones/borrar.php" class="boton-rojo-block">Eliminar</a>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $instalacion['id'];?>">
+                            <input type="submit" value="eliminar" class="boton-rojo-block">
+                        </form>
+                        
                         <a href="admin/instalaciones/actualizar.php?id=<?php echo $instalacion['id'] ?>" class="btn-amarillo">Actualizar</a>
                     </td>
                 </tr>
